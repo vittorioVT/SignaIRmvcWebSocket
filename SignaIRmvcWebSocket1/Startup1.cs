f954cd14f0b6b7f948f23b7038430d1c1d7119c4
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-09-19 22:10:21 +0300

    Connected with Hub

---
+++ SignaIRmvcWebSocket1/Startup1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignaIRmvcWebSocket1.Startup1))]
+
+namespace SignaIRmvcWebSocket1
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
